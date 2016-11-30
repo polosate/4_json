1a703e443cf7ee2f07641062a2512996d3ff1dbb
ashaposhnikova <polosate@gmail.com>
2016-11-30 17:08:57 +0300

    Add function for pretty printing json

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding="utf-8") as data_file:
+        data = json.loads(data_file.read())
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('alco_shops.json')
+    pretty_print_json(data)
